a5879af736869ead3303615695673b0263664972
Henry Till <henrytill@gmail.com>
2025-08-15 17:36:36 -0700

    remove Basic

--- Bits.lean
+++ Bits.lean
@@ -1,6 +1,5 @@
 -- This module serves as the root of the `Bits` library.
 -- Import modules here that should be built as part of the library.
-import Bits.Basic
 import Bits.Goedel
 import Bits.StackMachine
 import Bits.Fpil.Structures
