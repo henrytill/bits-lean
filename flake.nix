7b78b7c908bbb509d37d319b0d62b7004d8100d6
Henry Till <henrytill@gmail.com>
2025-11-23 16:07:16 -0800

    flake.nix: add package definition

--- flake.nix
+++ flake.nix
@@ -19,8 +19,26 @@
         };
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "bits-lean";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [ lean4 ];
+
+          buildPhase = ''
+            lake build
+            lake pack build-archive.tar.gz
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            tar -xzf build-archive.tar.gz -C $out
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ lean4 ];
+          inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [ yaml-language-server ];
         };
       }
     );
